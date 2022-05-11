abc3d8f49f028561859beab8a4dccf469cf8b42f
justintai1022 <79356462+justintai1022@users.noreply.github.com>
2022-05-11 11:15:02 +0800

    Create otlpcollector_example-2.py
    example code

---
+++ example_code_python/otlpcollector_example-2.py
@@ -0,0 +1,22 @@
+from faulthandler import enable
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+    OTLPSpanExporter,
+)
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+
+resource = Resource.create({SERVICE_NAME: "test-service-2"})
+span_exporter = OTLPSpanExporter()
+
+tracer_provider = TracerProvider(resource=resource)
+trace.set_tracer_provider(tracer_provider)
+span_processor = BatchSpanProcessor(span_exporter)
+tracer_provider.add_span_processor(span_processor)
+
+# Configure the tracer to use the collector exporter
+tracer = trace.get_tracer_provider().get_tracer(__name__)
+
+with tracer.start_as_current_span("foo"):
+    print("Hello world!")
